82e3ecb89de2cc92eb71fd9c3a14520fd9c60f05
Victor Hermes <me.victorhermes@gmail.com>
2019-10-02 15:25:50 -0300

    more-than-one

--- routes/pdf.js
+++ routes/pdf.js
@@ -3,24 +3,31 @@ const fs = require('fs');
 const router = express.Router()
 const PDFDocument = require('pdfkit')
 
+const students = [
+  { filename: 'John' },
+  { filename: 'Jane' }
+]
+
 router.post('/', (req, res) => {
-  const doc = new PDFDocument({
-    layout : 'landscape'
-    });
-    
-  let filename = req.body.filename
-  let content = req.body.content
-  doc.pipe(fs.createWriteStream(`public/pdf/${filename}.pdf`));
+  
+
+  students.forEach(s => {
+    const doc = new PDFDocument({
+      layout : 'landscape'
+      });
+      
+    doc.pipe(fs.createWriteStream(`public/pdf/${s.filename}.pdf`));
   // Stripping special characters
   //filename = encodeURIComponent(filename) + '.pdf'
   // Setting response to 'attachment' (download).
   // If you use 'inline' here it will automatically open the PDF
   //res.setHeader('Content-disposition', 'inline; filename="' + filename + '"')
   //res.setHeader('Content-type', 'application/pdf')
-  doc.fontSize(40).text(content, { align: 'center' }, 315);
+  doc.fontSize(40).text(s.filename, { align: 'center' }, 315);
   doc.image('public/images/cert.png', 10, 35, {width: 770});
-  doc.pipe(res)
-  doc.end()
+  doc.pipe(res);
+  doc.end();
+  });
 })
 
 module.exports = router
