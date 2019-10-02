d6074810883c4bf46fbf1e8e46bc77d80da3ae35
Victor Hermes <me.victorhermes@gmail.com>
2019-10-02 11:50:41 -0300

    save inside folder

--- routes/pdf.js
+++ routes/pdf.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const fs = require('fs');
 const router = express.Router()
 const PDFDocument = require('pdfkit')
 
@@ -9,12 +10,13 @@ router.post('/', (req, res) => {
     
   let filename = req.body.filename
   let content = req.body.content
+  doc.pipe(fs.createWriteStream(`public/pdf/${filename}.pdf`));
   // Stripping special characters
-  filename = encodeURIComponent(filename) + '.pdf'
+  //filename = encodeURIComponent(filename) + '.pdf'
   // Setting response to 'attachment' (download).
   // If you use 'inline' here it will automatically open the PDF
-  res.setHeader('Content-disposition', 'inline; filename="' + filename + '"')
-  res.setHeader('Content-type', 'application/pdf')
+  //res.setHeader('Content-disposition', 'inline; filename="' + filename + '"')
+  //res.setHeader('Content-type', 'application/pdf')
   doc.fontSize(40).text(content, { align: 'center' }, 315);
   doc.image('public/images/cert.png', 10, 35, {width: 770});
   doc.pipe(res)
