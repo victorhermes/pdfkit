23c9560e1da02ec4473bc79e0742b15beaa50537
Victor <me.victorhermes@gmail.com>
2019-10-03 22:37:55 -0300

    pdf

--- routes/download.js
+++ routes/download.js
@@ -4,12 +4,40 @@ var archiver = require("archiver");
 const router = express.Router();
 
 const students = [
-  { filename: "Flavio_047298037519049063" },
-  { filename: "Jane_07778770537539839" },
-  { filename: "Joao_06017049756481219" },
-  { filename: "John_03773756643909971" },
-  { filename: "Marcos_00030717703315690237" },
-  { filename: "Victor_008337815985709884" }
+  { filename: "John" },
+  { filename: "Jane" },
+  { filename: "Victor" },
+  { filename: "Joao" },
+  { filename: "Jorge" },
+  { filename: "Paulo" },
+  { filename: "Gustavo" },
+  { filename: "Heitor" },
+  { filename: "Marcos" },
+  { filename: "Kris" },
+  { filename: "Cris" },
+  { filename: "Caio" },
+  { filename: "Luan" },
+  { filename: "Jairo" },
+  { filename: "Michel" },
+  { filename: "Nick" },
+  { filename: "Monique" },
+  { filename: "Marlon" },
+  { filename: "Joel" },
+  { filename: "Santana" },
+  { filename: "Wagner" },
+  { filename: "Loverson" },
+  { filename: "Flaviun" },
+  { filename: "Janilo" },
+  { filename: "Danilo" },
+  { filename: "Daniel" },
+  { filename: "Daniele" },
+  { filename: "Dani" },
+  { filename: "Donis" },
+  { filename: "Tino" },
+  { filename: "Tenor" },
+  { filename: "Janes" },
+  { filename: "Victors" },
+  { filename: "Joaos" }
 ];
 
 router.post("/", (req, res) => {
--- routes/pdf.js
+++ routes/pdf.js
@@ -8,20 +8,46 @@ const students = [
   { filename: "Jane" },
   { filename: "Victor" },
   { filename: "Joao" },
-  { filename: "Flavio" },
-  { filename: "Marcos" }
+  { filename: "Jorge" },
+  { filename: "Paulo" },
+  { filename: "Gustavo" },
+  { filename: "Heitor" },
+  { filename: "Marcos" },
+  { filename: "Kris" },
+  { filename: "Cris" },
+  { filename: "Caio" },
+  { filename: "Luan" },
+  { filename: "Jairo" },
+  { filename: "Michel" },
+  { filename: "Nick" },
+  { filename: "Monique" },
+  { filename: "Marlon" },
+  { filename: "Joel" },
+  { filename: "Santana" },
+  { filename: "Wagner" },
+  { filename: "Loverson" },
+  { filename: "Flaviun" },
+  { filename: "Janilo" },
+  { filename: "Danilo" },
+  { filename: "Daniel" },
+  { filename: "Daniele" },
+  { filename: "Dani" },
+  { filename: "Donis" },
+  { filename: "Tino" },
+  { filename: "Tenor" },
+  { filename: "Janes" },
+  { filename: "Victors" },
+  { filename: "Joaos" }
 ];
 
 router.post("/", (req, res) => {
   try {
-    students.forEach(s => {
+    students.map(s => {
       const doc = new PDFDocument({
         layout: "landscape"
       });
 
-      const pdfStream = fs.createWriteStream(
-        `public/pdf/${s.filename}_${Math.random()}.pdf`
-      );
+      const pdfStream = fs.createWriteStream(`public/pdf/${s.filename}.pdf`);
 
       // Stripping special characters
       //filename = encodeURIComponent(filename) + '.pdf'
@@ -36,8 +62,6 @@ router.post("/", (req, res) => {
     });
   } catch (err) {
     console.error("MakePDF ERROR: " + err.message);
-  } finally {
-    res.redirect("/");
   }
 });
 
