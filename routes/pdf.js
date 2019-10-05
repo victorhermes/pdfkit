749225616eff207410716e30d15c83fff64320db
Victor <me.victorhermes@gmail.com>
2019-10-04 21:30:05 -0300

    pdf

--- routes/pdf.js
+++ routes/pdf.js
@@ -30,7 +30,6 @@ const students = [
   { filename: "Janilo" },
   { filename: "Danilo" },
   { filename: "Daniel" },
-  { filename: "Daniele" },
   { filename: "Dani" },
   { filename: "Donis" },
   { filename: "Tino" },
